0125f10b4d198278df6a039db2877ac2720ed1fa
Ruchira Madhushan Rajapaksha <52396694+LordMaduz@users.noreply.github.com>
2024-10-20 23:02:47 +0800

    Delete Mongo DB Auditing/src/main/java/com/audit/mongo/registry directory

--- Mongo DB Auditing/src/main/java/com/audit/mongo/registry/support/InvalidSchemaException.java
+++
@@ -1,7 +0,0 @@
-package com.audit.mongo.schema.registry.support;
-
-public class InvalidSchemaException extends RuntimeException{
-    public InvalidSchemaException(String message) {
-        super(message);
-    }
-}
